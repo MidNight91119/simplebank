b229a66da9ac2dacefd1c3c89913cf429f44a420
Ayush Bhagwat <ayushbhagwat921@gmail.com>
2026-07-02 11:24:38 +0530

    Implemented transfer money API with a custom params validator

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	db "github.com/MidNight91119/simplebank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 // Server serves HTTP requests for our banking services
@@ -16,18 +18,21 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	// registering our custom validator with gin
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	// Add routes to router
 	// if you pass multiple functions then the last one should be the handler
 	// and in between should be middlewares
 	router.POST("/accounts", server.createAccount)
-
 	router.GET("/accounts/:id", server.getAccount)
-
 	router.GET("/accounts", server.listAccounts)
-
 	router.DELETE("/accounts/:id", server.deleteAccount)
-
 	router.PUT("/accounts/:id", server.updateAccount)
+
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
