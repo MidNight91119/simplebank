6a60d80bf1f8f18fc189370babef60c4e6c4481d
Ayush Bhagwat <ayushbhagwat921@gmail.com>
2026-07-25 18:14:20 +0530

    Embed static front-end files inside Go backend server's binary
    Signed-off-by: Ayush Bhagwat <ayushbhagwat921@gmail.com>

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"database/sql"
+	"embed"
+	"io/fs"
 	"log"
 	"net"
 	"net/http"
@@ -18,6 +20,9 @@ import (
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/encoding/protojson"
 )
+
+//go:embed doc/swagger
+var swaggerFS embed.FS
 
 func main() {
 	config, err := util.LoadConfig(".")
@@ -57,8 +62,13 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
-	fs := http.FileServer(http.Dir("./doc/swagger"))
-	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+	swaggerSub, err := fs.Sub(swaggerFS, "doc/swagger")
+	if err != nil {
+		log.Fatal("cannot create sub filesystem: ", err)
+	}
+
+	fileServer := http.FileServer(http.FS(swaggerSub))
+	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fileServer))
 
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
