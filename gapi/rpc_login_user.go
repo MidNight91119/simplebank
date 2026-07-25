2b8d53854d74d1e78af4f3f7679579b981c95317
Ayush Bhagwat <72154731+MidNight91119@users.noreply.github.com>
2026-07-25 15:43:13 +0530

    Extract user agent and client IP from gRPC metadata (#12)
    Signed-off-by: Ayush Bhagwat <ayushbhagwat921@gmail.com>

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -42,12 +42,14 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "failed to create refresh token: %s", err)
 	}
 
+	mtdt := server.extractMetadata(ctx)
+
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           refreshPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIP:     "",
+		UserAgent:    mtdt.UserAgent,
+		ClientIP:     mtdt.ClientIP,
 		ExpiresAt:    refreshPayload.ExpiredAt,
 	})
 	if err != nil {
