b43a5dca96e8be3d6490eb11d38f4f7759fa5f36
Ayush Bhagwat <ayushbhagwat921@gmail.com>
2026-07-27 20:30:59 +0530

    HTTP logger middleware
    Signed-off-by: Ayush Bhagwat <ayushbhagwat921@gmail.com>

--- gapi/logger.go
+++ gapi/logger.go
@@ -2,6 +2,7 @@ package gapi
 
 import (
 	"context"
+	"net/http"
 	"time"
 
 	"github.com/rs/zerolog/log"
@@ -9,6 +10,22 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
+
+type ResponseRecorder struct {
+	http.ResponseWriter
+	StatusCode int
+	Body       []byte
+}
+
+func (rec *ResponseRecorder) Write(body []byte) (int, error) {
+	rec.Body = append(rec.Body, body...)
+	return rec.ResponseWriter.Write(body)
+}
+
+func (rec *ResponseRecorder) WriteHeader(statusCode int) {
+	rec.StatusCode = statusCode
+	rec.ResponseWriter.WriteHeader(statusCode)
+}
 
 func GrpcLogger(
 	ctx context.Context,
@@ -38,4 +55,29 @@ func GrpcLogger(
 		Msg("received a gRPC request")
 
 	return result, err
+}
+
+func HttpLogger(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		startTime := time.Now()
+		rec := &ResponseRecorder{
+			ResponseWriter: res,
+			StatusCode:     http.StatusOK,
+		}
+		handler.ServeHTTP(rec, req)
+		duration := time.Since(startTime)
+
+		logger := log.Info()
+		if rec.StatusCode != http.StatusOK {
+			logger = log.Error().Bytes("body", rec.Body)
+		}
+
+		logger.Str("protocol", "http").
+			Str("method", req.Method).
+			Str("path", req.RequestURI).
+			Int("status_code", rec.StatusCode).
+			Str("status_text", http.StatusText(rec.StatusCode)).
+			Dur("duration", duration).
+			Msg("received an HTTP request")
+	})
 }
