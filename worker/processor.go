ffffef51e986732eed1af1f4476bc567602819dc
Ayush Bhagwat <72154731+MidNight91119@users.noreply.github.com>
2026-08-22 15:17:02 +0530

    handle errors and print logs for Async workers (#24)
    Signed-off-by: Ayush Bhagwat <ayushbhagwat921@gmail.com>

--- worker/processor.go
+++ worker/processor.go
@@ -5,6 +5,8 @@ import (
 
 	db "github.com/MidNight91119/simplebank/db/sqlc"
 	"github.com/hibiken/asynq"
+	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -23,6 +25,7 @@ type RedisTaskProcessor struct {
 }
 
 func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
+	redis.SetLogger(NewLogger())
 	server := asynq.NewServer(
 		redisOpt,
 		asynq.Config{
@@ -30,6 +33,11 @@ func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskPr
 				QueueCritical: 10,
 				QueueDefault:  5,
 			},
+			Logger: NewLogger(),
+			ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+				log.Error().Err(err).Str("type", task.Type()).
+					Bytes("payload", task.Payload()).Msg("process task failed")
+			}),
 		},
 	)
 
