495fcf0795ee94fee3c7f53075cbaa3e1c2b5694
Ayush Bhagwat <ayushbhagwat921@gmail.com>
2026-08-22 16:56:32 +0530

    Send emails with Gmail in Go
    Signed-off-by: Ayush Bhagwat <ayushbhagwat921@gmail.com>

---
+++ mail/sender_test.go
@@ -0,0 +1,28 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/MidNight91119/simplebank/db/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email"
+	content := `
+	<h1>Hello world</h1> <p>This is a test message from Mid Night</p>
+	`
+	to := []string{"ayushbhagwat921@gmail.com"}
+	attachFiles := []string{"../CLAUDE.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
