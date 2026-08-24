3663340b9f00f3d82c99e8cd51012334c395c4e0
Ayush Bhagwat <ayushbhagwat921@gmail.com>
2026-08-24 05:57:38 +0530

    Set up CORS policy and move to math/rand/v2
    Signed-off-by: Ayush Bhagwat <ayushbhagwat921@gmail.com>

--- db/util/random.go
+++ db/util/random.go
@@ -2,20 +2,15 @@ package util
 
 import (
 	"fmt"
-	"math/rand"
+	"math/rand/v2"
 	"strings"
-	"time"
 )
 
 const alphabets = "abcdefghijklmnopqrstuvwxyz"
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
 // RandomInt generates random integer between min and max
 func RandomInt(min, max int64) int64 {
-	return min + rand.Int63n(max-min+1)
+	return min + rand.Int64N(max-min+1)
 }
 
 // RandomString generates a random string of length n
@@ -24,7 +19,7 @@ func RandomString(n int) string {
 	k := len(alphabets)
 
 	for i := 0; i < n; i++ {
-		c := alphabets[rand.Intn(k)]
+		c := alphabets[rand.IntN(k)]
 		sb.WriteByte(c)
 	}
 
@@ -45,7 +40,7 @@ func RandomMoney() int64 {
 func RandomCurrency() string {
 	currencies := []string{USD, EUR, CAD}
 	n := len(currencies)
-	return currencies[rand.Intn(n)]
+	return currencies[rand.IntN(n)]
 }
 
 // RandomEmail generates a new random email
